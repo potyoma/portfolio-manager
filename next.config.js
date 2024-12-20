f57d68069c9a5439dec29c34174ed4613a7a8998
Artem Novoselov <artyomnovosyolov254@gmail.com>
2024-12-20 14:31:08 +0100

    feat: enable docker

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  reactStrictMode: true,
+  output: "standalone",
   experimental: {
     reactCompiler: true,
   },
